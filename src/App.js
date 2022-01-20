e9aa13e48dbd9e54382b6d39e2abe4a617308726
Denver-Meadows <meadows8200@gmail.com>
2022-01-19 21:06:56 -0500

    testing hero

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 function App() {
-  return <Navbar />;
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Hero.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../styles/hero.scss";
+
+const Hero = () => {
+  return <div className="hero"></div>;
+};
+
+export default Hero;
