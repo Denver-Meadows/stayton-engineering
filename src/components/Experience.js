7257e8ec66aff3bb956cb2b0a1a0312de74f1365
Denver-Meadows <meadows8200@gmail.com>
2022-01-30 11:17:07 -0500

    experience component updated

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,15 +1,7 @@
 import React from "react";
 import "../styles/experience.scss";
 import Fade from "react-reveal/Fade";
-
-import allen from "../imgs/logos/allen-bradley.jpeg";
-import bosch from "../imgs/logos/bosch.png";
-import ge from "../imgs/logos/ge.png";
-import mitsubishi from "../imgs/logos/mitsubishi.png";
-import proface from "../imgs/logos/proface.png";
-import redlion from "../imgs/logos/redlion.png";
-import siemens from "../imgs/logos/siemens.png";
-import wonderware from "../imgs/logos/wonderware.png";
+import { experienceData } from "../data/experienceData";
 
 const Experience = () => {
   return (
@@ -18,41 +10,15 @@ const Experience = () => {
         <h1>Experience</h1>
       </div>
       <div className="experience-list">
-        <div className="img-container">
-          <Fade bottom>
-            <img src={ge} alt="GE Logo" />
-          </Fade>
-        </div>
-        <div className="img-container">
-          <Fade bottom>
-            <img src={bosch} alt="Bosch Logo" />
-          </Fade>
-        </div>
-        <div className="img-container">
-          <img src={proface} alt="Proface Logo" />
-        </div>
-        <div className="img-container">
-          <img src={redlion} alt="RedLion Logo" />
-        </div>
-        <div className="img-container">
-          <img src={allen} alt="Allen Logo" />
-        </div>
-        <div className="img-container">
-          <img src={mitsubishi} alt="Mitsubishi Logo" />
-        </div>
-        <div className="img-container">
-          <img src={wonderware} alt="Wonderware Logo" />
-        </div>
-        <div className="img-container">
-          <img src={siemens} alt="Siemens Logo" />
-        </div>
-
-        {/* <div className="experience-list-2">
-          <img src={allen} alt="Allen Logo" />
-          <img src={mitsubishi} alt="Mitsubishi Logo" />
-          <img src={wonderware} alt="Wonderware Logo" />
-          <img src={siemens} alt="Siemens Logo" />
-        </div> */}
+        {experienceData.map((experience, index) => {
+          return (
+            <div className="img-container" key={index}>
+              <Fade bottom>
+                <img src={experience.img} alt={experience.alt} />
+              </Fade>
+            </div>
+          );
+        })}
       </div>
     </section>
   );
---
+++ src/data/experienceData.js
@@ -0,0 +1,43 @@
+import allen from "../imgs/logos/allen-bradley.jpeg";
+import bosch from "../imgs/logos/bosch.png";
+import ge from "../imgs/logos/ge.png";
+import mitsubishi from "../imgs/logos/mitsubishi.png";
+import proface from "../imgs/logos/proface.png";
+import redlion from "../imgs/logos/redlion.png";
+import siemens from "../imgs/logos/siemens.png";
+import wonderware from "../imgs/logos/wonderware.png";
+
+export const experienceData = [
+  {
+    img: allen,
+    alt: "Allen Logo",
+  },
+  {
+    img: bosch,
+    alt: "Bosch Logo",
+  },
+  {
+    img: ge,
+    alt: "GE Logo",
+  },
+  {
+    img: mitsubishi,
+    alt: "Mitsubishi Logo",
+  },
+  {
+    img: proface,
+    alt: "Proface Logo",
+  },
+  {
+    img: redlion,
+    alt: "RedLion Logo",
+  },
+  {
+    img: siemens,
+    alt: "Siemens Logo",
+  },
+  {
+    img: wonderware,
+    alt: "Wonderware Logo",
+  },
+];
